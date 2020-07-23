119fefd2b93cbae212803a40b988291822efebe9
useproject <49857721+useproject@users.noreply.github.com>
2020-07-23 15:30:10 +0800

    Update fsnotify.go

--- fsnotify.go
+++ fsnotify.go
@@ -17,7 +17,7 @@ import (
 type Event struct {
 	Name string // Relative path to the file or directory.
 	Op   Op     // File operation that triggered the event.
-	Pid  uint32 
+	Pid  int 
 }
 
 // Op describes a set of file operations.
